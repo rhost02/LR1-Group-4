14873ffa4607416421ac8c07077706204ed8d3ef
Rayne <125546682+RyanRayLimbo@users.noreply.github.com>
2023-03-03 16:45:59 +0800

    Added the mean function and code for the input

--- statistician.py
+++ statistician.py
@@ -1,1 +1,10 @@
+def meanValue(rawData):
+  mean = sum(rawData)/len(rawData)
+  
+  return print("The mean is: ", mean)
 
+inputData = list(map(int, input("Enter the data: ").split()))
+inputData.sort()
+print("Data: ", inputData)
+
+meanValue(inputData)
