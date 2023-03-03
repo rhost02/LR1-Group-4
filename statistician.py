d942d15fae5c1eb9a47e51f8916f8d3ea6709f8a
rhost02 <125547024+rhost02@users.noreply.github.com>
2023-03-03 17:47:49 +0800

    Update statistician.py

--- statistician.py
+++ statistician.py
@@ -1,17 +1,18 @@
 name = "Group 4/B1"
 print("Group N# & section is: " + name)
 
+def medianValue(rawData):
+    dataLength = len(rawData)
+    mid = dataLength // 2
+    if dataLength % 2 == 0:
+        median = (rawData[mid - 1] + rawData[mid]) / 2
+    else:
+        median = rawData[mid]
+        
+    return print("The median is: ", median)
+    
 def meanValue(rawData):
-  mean = sum(rawData)/len(rawData)
-  
-  return print("The mean is: ", mean)
-
-inputData = list(map(int, input("Enter the data: ").split()))
-inputData.sort()
-print("Data: ", inputData)
-
-meanValue(inputData):
-  mean = sum(rawData) / len(rawData)
+    mean = sum(rawData) / len(rawData)
     
     return print("The mean is: ", mean)
     
